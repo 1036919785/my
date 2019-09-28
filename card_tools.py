b74bb7e3a7bb5ac839964c4c94d91f080c28aa3f
jiege <1036@qq.com>
2019-09-28 18:09:03 +0800

    我的python

--- card_tools.py
+++ card_tools.py
@@ -50,10 +50,10 @@ def show_cards():
         print("没有名片信息!")
         return
 
-    for name in ["姓名","qq","phone","email"]:
-        print(name,end="\t\t")
+    for name in ["姓名", "qq", "phone", "email"]:
+        print(name, end="\t\t")
     print("")
-    print("="*50)
+    print("=" * 50)
 
     for card_dict in card_list:
         print("%s\t\t%s\t\t%s\t\t%s" % (card_dict["name"],
@@ -76,7 +76,46 @@ def search_card():
             print("找到了%s" % find_name)
             print(card_dict)
             # TODO 找到后需要执行的操作
+            deal_card(card_dict)
             break
 
     else:
         print("没有找到%s" % find_name)
+
+
+def deal_card(dict_card):
+    # print(dict_card)
+    action_str = input("请输入要执行的操作:[1]修改  [2]删除  [0]退出")
+    if action_str == "1":
+
+        # TODO 修改名片
+        print("修改")
+        dict_card["name"] = input_info_card(dict_card["name"],"name:")
+        dict_card["phone"] = input_info_card(dict_card["phone"],"phone:")
+        dict_card["qq"] = input_info_card(dict_card["qq"],"qq:")
+        dict_card["email"] = input_info_card(dict_card["email"],"name:")
+    elif action_str == "2":
+
+        # TODO 删除名片
+        card_list.remove(dict_card)
+        print("删除")
+
+
+def input_info_card(dict_value,tip_message):
+    """
+
+    :param dict_value:
+    :param tip_message:
+    :return:
+    """
+    #提示用户输入内容
+    input_str = input(tip_message)
+    #判断是否输入
+    if len(input_str) == 0:
+        return dict_value
+    else:
+        return input_str
+
+
+
+
